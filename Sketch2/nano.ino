66c64360b00fe31afc5a898550884bce4678f5e1
Felix Lemke <flemke.dev@gmail.com>
2017-02-14 21:16:09 +0100

    Added "Nano" Project

---
+++ Sketch2/nano.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch2.ino
+ Created:	14.02.2017 21:15:23
+ Author:	felix
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
